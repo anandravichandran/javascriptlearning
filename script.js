e16644574f9b9e36de002bca5863c364602e86a5
anandravichandran <anandravichandran127@gmail.com>
2024-11-21 21:45:07 +0530

    "today learning"

---
+++ script.js
@@ -0,0 +1,52 @@
+// alert("welcome to home");
+
+// 4.instead we are writing h1 in js 
+// document.write("Welcome to Tevexxo");
+
+/*  in the console which is developer tool
+     console.log("hello");
+     o/p --> hello
+
+     alert("hello");
+     it will alert in the web browser
+
+     document.write("hello");
+     it will show hello in the web browser
+*/
+
+// variable:
+
+// let --> create new variable
+// variable_name --> variable name 
+
+// variable name restrictions:
+// 1. cant use special words Example: let --> let let =3; it shows an error we can use another method were as let let1 = 3;
+
+//2. cant start with a number , example: let na2me = 3; it shows an error --> were as use another type let name2 = 3;
+
+//3. cant use special character : excpet:$ _ 
+// example : let name@!#%^&* = 3 it wrong 
+// let name_two = 4; it is correct
+// let _name = 3; (or) let $name = 3;
+
+
+let variable_name = 3;
+console.log(variable_name);
+
+let calculation = 2+2;
+console.log(calculation)
+console.log(calculation+2);
+
+let result = calculation +2;
+console.log(result);
+
+let message = 'helloe';
+console.log(message); console.log(';');
+
+ variable1 = 5;
+console.log(variable1);
+ variable1 = variable1 + 1;
+ console.log(variable1);
+
+
+// why use semicolon ? it say this statement is end ---  if we remove the semicolon it show an error uncaught syntax error
